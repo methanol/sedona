2155efb25d49aaff9fd6822927a00b1d12f99bbd
methanol <methanol.b@gmail.com>
2018-05-05 16:42:20 +0700

    создание svg-спрайта
    создал свг-спрайт(пока внутри html). поправил стили иконок.

--- source/js/nav-hiding.js
+++ source/js/nav-hiding.js
@@ -2,6 +2,8 @@
 (function () {
   var toggler = document.querySelector('.main-nav__toggle');
   var nav = document.querySelector('.main-nav');
+  var cross = document.querySelector('.main-nav__toggle-close');
+  var ham = document.querySelector('.main-nav__toggle-open');
 
   nav.classList.remove('main-nav--nojs');
 
@@ -9,9 +11,13 @@
     if (nav.classList.contains('main-nav--open')) {
       nav.classList.remove('main-nav--open');
       nav.classList.add('main-nav--close');
+      ham.style.display = 'none';
+      cross.style.display = 'block';
     } else {
       nav.classList.add('main-nav--open');
       nav.classList.remove('main-nav--close');
+      ham.style.display = 'block';
+      cross.style.display = 'none';
     }
   });
 
